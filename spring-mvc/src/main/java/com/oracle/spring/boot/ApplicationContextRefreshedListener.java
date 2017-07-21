3005030172deb0777ff18e65998ad348b5924e42
zhangtao0130 <36641572@qq.com>
2017-07-21 12:12:50 +0800

    Update ApplicationContextRefreshedListener.java

--- spring-mvc/src/main/java/com/oracle/spring/boot/ApplicationContextRefreshedListener.java
+++ spring-mvc/src/main/java/com/oracle/spring/boot/ApplicationContextRefreshedListener.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
 
 /**
- * 测试spring_projects...........
+ * 测试spring_projects............................................................
  */
 @Component
 public class ApplicationContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
